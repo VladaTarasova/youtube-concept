fc9957b0f84ef0513c8675d5235ba7a06b3b7a23
VladaTarasova <88935116+VladaTarasova@users.noreply.github.com>
2021-12-27 19:11:52 +0300

    Добавила правила для адаптива

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-const swiper1 = new Swiper('.channel__slider', {
+const channelSlider = new Swiper('.channel__slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    }
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-swiper-button-next',
@@ -9,10 +27,19 @@ const swiper1 = new Swiper('.channel__slider', {
   },
 });
 
-const swiper2 = new Swiper('.recommendations__slider', {
+const recommendationsSlider = new Swiper('.recommendations__slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3
+    },
+    1100: {
+      slidesPerView: 2
+    }
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.recommendations-swiper-button-next',
@@ -20,13 +47,43 @@ const swiper2 = new Swiper('.recommendations__slider', {
   },
 });
 
-const swiper3 = new Swiper('.recommended-channel__slider', {
+const recommendedChannelSlider = new Swiper('.recommended-channel__slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    }
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.recommended-channel-swiper-button-next',
     prevEl: '.recommended-channel-swiper-button-prev',
   },
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.header__input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrolWidth <= 640) {
+  channelSlider.destroy();
+  recommendationsSlider.destroy();
+  recommendedChannelSlider.destroy();
+}
